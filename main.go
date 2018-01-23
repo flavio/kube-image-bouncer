d27ab50ea7053e1031438684c873d52d4b139103
Flavio Castelli <fcastelli@suse.com>
2018-01-23 11:25:33 +0100

    Better flag handling

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"github.com/labstack/gommon/log"
+	"gopkg.in/urfave/cli.v1"
 
 	"github.com/flavio/kube-image-bouncer/handlers"
 )
@@ -15,28 +16,64 @@ import (
 func main() {
 	var cert, key string
 	var port int
+	var debug bool
 
-	flag.IntVar(&port, "port", 1323, "Port number to listen to")
-	flag.StringVar(&cert, "cert", "server.cert", "TLS certificate file")
-	flag.StringVar(&key, "key", "server.key", "TLS key")
+	app := cli.NewApp()
+	app.Name = "kube-image-bouncer"
+	app.Usage = "webhook endpoint for kube image policy admission controller"
 
-	flag.Parse()
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "cert, c",
+			Value:       "server.cert",
+			Usage:       "Path to the certificate to use",
+			EnvVar:      "BOUNCER_CERTIFICATE",
+			Destination: &cert,
+		},
+		cli.StringFlag{
+			Name:        "key, k",
+			Value:       "server.key",
+			Usage:       "Path to the key to use",
+			EnvVar:      "BOUNCER_KEY",
+			Destination: &key,
+		},
+		cli.IntFlag{
+			Name:        "port, p",
+			Value:       1323,
+			Usage:       "`PORT` to listen to",
+			EnvVar:      "BOUNCER_PORT",
+			Destination: &port,
+		},
+		cli.BoolFlag{
+			Name:        "debug",
+			Usage:       "Enable extra debugging",
+			EnvVar:      "BOUNCER_DEBUG",
+			Destination: &debug,
+		},
+	}
 
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.POST("/policy", handlers.PostImagePolicy())
+	app.Action = func(c *cli.Context) error {
+		e := echo.New()
+		e.GET("/", func(c echo.Context) error {
+			return c.String(http.StatusOK, "Hello, World!")
+		})
+		e.POST("/policy", handlers.PostImagePolicy())
 
-	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-		Format: "method=${method}, uri=${uri}, status=${status}\n",
-	}))
+		e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+			Format: "method=${method}, uri=${uri}, status=${status}\n",
+		}))
 
-	e.Logger.SetLevel(log.DEBUG)
+		if debug {
+			e.Logger.SetLevel(log.DEBUG)
+		}
 
-	e.Logger.Fatal(
-		e.StartTLS(
-			fmt.Sprintf(":%d", port),
-			cert,
-			key))
+		err := e.StartTLS(fmt.Sprintf(":%d", port), cert, key)
+		if err != nil {
+			return cli.NewExitError(err, 1)
+		}
+
+		return nil
+	}
+
+	app.Run(os.Args)
 }
